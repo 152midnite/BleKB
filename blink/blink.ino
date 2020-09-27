8f31dc7f382842ce686cb8b8d668dabc07e379bc
dante <p.k.golabek@gmail.com>
2020-09-27 04:43:14 +0200

    added sweep

--- blink/blink.ino
+++ blink/blink.ino
@@ -12,22 +12,40 @@ void setup() {
 	bleKeyboard.begin();
 }
 
+
+
+void sweep(int in_pins[], int out_pins[], char keys[][13]){
+	int i, j, n_rows = sizeof(out_pins);
+	for (i=0;i<n_rows;i++){
+		digitalWrite(out_pins[i], HIGH);
+		int row_lenght = sizeof(keys[i]);
+		for (j=0;j<row_lenght;j++){
+			char key = keys[i][j];
+			if(key){
+			int pin = in_pins[i];
+			if(digitalRead(pin) == HIGH){
+				bleKeyboard.press(key);
+        Serial.println("Sending key");
+			}
+			else{
+				bleKeyboard.release(keys[i][j]);}
+			}
+		}
+	}
+}
+
+int in_pins[13] = {};
+int out_pins[4] = {};
+char keys[4][13] = {{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'},
+										{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'},
+										{'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'},
+										{}};
 void loop() {
   if(bleKeyboard.isConnected()) {
-    Serial.println("Sending 'Hello world'...");
-		digitalWrite(17, HIGH);
-		if(digitalRead(34) == HIGH){
-				Serial.println("writing a");
-				bleKeyboard.press('c');
-				delay(10);}
-		else{bleKeyboard.release('c');}
-		
-		if(digitalRead(35) == HIGH){
-				Serial.println("writing a");
-				bleKeyboard.press(0x62);
-				delay(10);}
-		else{bleKeyboard.release(0x62);}
+		sweep(in_pins, out_pins, keys);	
 
+
+	
 /*
     bleKeyboard.print("Hello world");
     bleKeyboard.write(KEY_RETURN);
@@ -39,4 +57,5 @@ void loop() {
 */
   }
   Serial.println("Waiting 5 seconds...");
+	delay(5000);
 }
