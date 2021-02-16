5304f1b658f59a1055a6755f47c30b463896b7a4
dante <p.k.golabek@gmail.com>
2021-02-16 19:27:45 +0100

    some working stuff

--- blink/blink.ino
+++ blink/blink.ino
@@ -3,40 +3,17 @@
 BleKeyboard bleKeyboard;
 
 
-int output_pins(int out_pins[4]) {
-	return 0;
-  }
-
-
-int sweep(int in_pins[], int out_pins[], char keys[][13]) {
-  int i, j, n_rows = sizeof(out_pins);
-  for (i = 0; i < n_rows; i++) {
-    digitalWrite(out_pins[i], HIGH);
-    int row_lenght = sizeof(keys[i]);
-    for (j = 0; j < row_lenght; j++) {
-      char key = keys[i][j];
-      if (key) {
-        int pin = in_pins[i];
-        if (digitalRead(pin) == HIGH) {
-          bleKeyboard.press(key);
-          Serial.println("Sending key");
-        }
-        else {
-          bleKeyboard.release(keys[i][j]);
-        }
-      }
-    }
-    digitalWrite(out_pins[i], LOW);
-  }
-return 0;}
-
-
 int in_pins[13] = {36,39,34,35,23,19,21,22};
 int out_pins[10] = {5,13,12,4,32,33,25,26,27,14};
-char keys[4][13] = {{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'},
-                    {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'},
-                    {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+char keys[8][6] = {{'q', 'w', 'e', 'r', 't', 'y'}, 
+										{'u', 'i', 'o', 'p', '[', ']'},
+                    {KEY_TAB, 'a', 's', 'd', 'f', 'g'}, 
+									  {'h', 'j', 'k', 'l', ';', KEY_RETURN},
+                    {'/', 'z', 'x', 'c', 'v', 'b'}, 
+										{'n', 'm', ',', '.', '/', KEY_RIGHT_SHIFT},
+                    {' ', ' ', ' ', ' ', ' ', ' '},
+										{KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT,
+										 KEY_LEFT_SHIFT, ' ', ' '}};
 
 
 
@@ -57,24 +34,37 @@ void setup() {
   Serial.println("Starting BLE work!");
   bleKeyboard.begin();
 	int n = sizeof(out_pins);
+	int m = sizeof(in_pins);
 	int i, j;
-  for (j=0;j<n;j++){pinMode(out_pins[j], OUTPUT);}
-  for (i=0;i<n;i++){digitalWrite(out_pins[i], HIGH);}
+	
+
+  for (i=0;i<n;i++){pinMode(out_pins[i], OUTPUT);}
+ // for (i=0;i<n;i++){digitalWrite(out_pins[i], HIGH);}
 }
 
 
 void loop() {
+	int n = sizeof(out_pins);
+	int m = sizeof(in_pins);
+	int i, j;
   if (bleKeyboard.isConnected()) {
-    //bleKeyboard.print("Hello world");
-		if (digitalRead(23) == HIGH){bleKeyboard.press('a');}
-		else{bleKeyboard.release('a');}
-		if (digitalRead(19) == HIGH){bleKeyboard.press('b');}
-		else{bleKeyboard.release('b');}
-		if (digitalRead(21) == HIGH){bleKeyboard.press('c');}
-		else{bleKeyboard.release('c');}
-		if (digitalRead(22) == HIGH){bleKeyboard.press(KEY_BACKSPACE);}
-		else{bleKeyboard.release(KEY_BACKSPACE);}
-		        //bleKeyboard.write('a');
+  	for (i=0;i<n;i++){
+			digitalWrite(out_pins[i], HIGH);
+			for(j=0;j<m;j++){
+				delay(10);
+				if(digitalRead(in_pins[j]) == HIGH){bleKeyboard.press(keys[j][i]);}
+				else{bleKeyboard.release(keys[j][i]);}
+			}
+			digitalWrite(out_pins[i], LOW);
+		}
+//		if (digitalRead(23) == HIGH){bleKeyboard.press('a');}
+//		else{bleKeyboard.release('a');}
+//		if (digitalRead(19) == HIGH){bleKeyboard.press('b');}
+//		else{bleKeyboard.release('b');}
+//		if (digitalRead(21) == HIGH){bleKeyboard.press('c');}
+//		else{bleKeyboard.release('c');}
+//		if (digitalRead(22) == HIGH){bleKeyboard.press(KEY_BACKSPACE);}
+//		else{bleKeyboard.release(KEY_BACKSPACE);}
   }
-  delay(5);
+
 }
